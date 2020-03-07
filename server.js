c5ab56ab6d0090ac6df18d5c7aa022da5ed98e03
fenix15100 <francisco.camacho15@gmail.com>
2020-03-07 07:46:34 +0100

    fix socket.io dependency

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 
+const http = require('http')
 const express = require('express');
 const path = require('path');
+const socketio = require('socket.io');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+// if declare express app without http.createServer() socket-io break according socket-io docs
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-
+//Enable cors for serve api
+app.use(cors());
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -14,7 +24,8 @@ app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
 
-console.log('App is listening on port ' + port);
+
+
+app.listen(port,()=>console.log('Server is ready in port:  ' + port));
+
