382a1965539c396d40262551fd92efef34a86c84
fenix15100 <francisco.camacho15@gmail.com>
2020-12-11 12:44:55 +0100

    refactor

--- server/index.js
+++ server/index.js
@@ -17,12 +17,12 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./UserControll
 app.use(cors());
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, './..client/build')));
 
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'./../client/build/index.html'));
 });
 
 
